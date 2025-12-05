d5f1a0dd62ffae407b8c6eb1a5f453cf21143242
Enya <6b4b5xbtvy@privaterelay.appleid.com>
2025-12-06 00:31:53 +0100

    Bugfixes fünfzehn

--- script.js
+++ script.js
@@ -227,7 +227,7 @@ if (nextButtonDark) {
             darkAnswers: JSON.parse(localStorage.getItem("darkAnswers") || "{}")
         };
 
-        fetch("https://script.google.com/macros/s/AKfycbwXK4HbqQQfr-1sXh0BRfEYXb6m590RLpJZXSXj2A2K7uXqk_MTwSbyiQgdt57NIQv6/exec", {
+        fetch("https://script.google.com/macros/s/AKfycbwYYDzvgxJXOljo-CS_rmPJZEV2bzCMIUxma-Z9g_2aKH9UqcsImXdmEM3_bOby4bGh/exec", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify(payload)
